813f591e9251b0b80d6e19773430949cd1d5651d
Edgar Lozano <edgar@edgarl.com>
2020-06-05 08:27:15 -0500

    Updates

---
+++ precache-manifest.a1bbd302e2579f5e44ff5fbb290852c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "212ab7eb995ee6b5e359955031a9cdd1",
+    "url": "/pomodoro/index.html"
+  },
+  {
+    "revision": "111b1f59806c97c7e94d",
+    "url": "/pomodoro/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "47b427a694769ebb3160",
+    "url": "/pomodoro/static/js/2.30307454.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro/static/js/2.30307454.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "111b1f59806c97c7e94d",
+    "url": "/pomodoro/static/js/main.f3b52a5f.chunk.js"
+  },
+  {
+    "revision": "baeacddfcfd8b4476a79",
+    "url": "/pomodoro/static/js/runtime-main.e57d07b9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pomodoro/static/media/logo.5d5d9eef.svg"
+  }
+]);
